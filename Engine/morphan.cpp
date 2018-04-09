0de64e7e94305307c6e7b559df7d826eab9499ba
alesapin <alesapin@gmail.com>
2018-04-09 22:43:45 +0300

    Change to uniqptr

--- Engine/morphan.cpp
+++ Engine/morphan.cpp
@@ -3,9 +3,9 @@
  QVector<WordForm> MorphAn::getWordInfo(const QString &word) const
 {
     QVector<WordForm> result;
-    std::auto_ptr<boost::ptr_vector<lspl::morphology::WordForm>> r = morph->getWordForms(utility::covertToWinCp(word));
-    for(int i = 0;i<r->size();++i){
-        WordForm current = convertWordForm(r->operator [](i));
+    std::unique_ptr<boost::ptr_vector<lspl::morphology::WordForm>> r(morph->getWordForms(utility::covertToWinCp(word)));
+    for(int i = 0; i < r->size(); ++i){
+        WordForm current = convertWordForm(r->operator[](i));
         result.push_back(current);
     }
     return result;
@@ -18,14 +18,14 @@ WordForm MorphAn::convertWordForm(const lspl::morphology::WordForm& wf) const
     WordForm result;
     result.speechPart = utility::convertToUnicode(wf.getSpeechPart().getName());
     result.nf = utility::convertToUnicode(wf.getBase());
-    for(int i = 0;i<wf.getAttributeSetCount();++i){
+    for(int i = 0; i < wf.getAttributeSetCount(); ++i) {
         QVector<QString> currentSet;
-        for(int j = 1;j<AttributeKey::count();++j){
+        for(int j = 1; j < AttributeKey::count(); ++j) {
             AttributeKey currkey(j);
             if(currkey == AttributeKey::BASE || currkey == AttributeKey::STEM) continue;
-            AttributeValue val = wf.getAttribute(i,currkey);
+            AttributeValue val = wf.getAttribute(i, currkey);
             if (val != AttributeValue::UNDEFINED) {
-                    QString stringVal = utility::convertToUnicode(/*currkey.getName() +" : "+*/val.getString());
+                    QString stringVal = utility::convertToUnicode(val.getString());
                     currentSet.push_back(stringVal);
               }
         }
